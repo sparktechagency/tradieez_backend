033969858c21b9a4846a635b97741305e7a68238
goni715 <halesgoni715149123@gmail.com>
2026-01-13 11:52:04 +0600

    ApplyJobService

--- src/services/application/ApplyJobService.ts
+++ src/services/application/ApplyJobService.ts
@@ -1,45 +1,60 @@
 import CustomError from "../../errors/CustomError";
 import { IApplication } from "../../interfaces/application.interface";
 import ApplicationModel from "../../models/ApplicationModel";
+import CandidateModel from "../../models/CandidateModel";
 import JobModel from "../../models/Job.Model";
 
 const ApplyJobService = async (loginUserId: string, payload: IApplication) => {
-    //check job
-    const job = await JobModel.findById(payload.jobId);
-    if (!job) {
-        throw new CustomError(404, 'Job not found with the provided ID.');
-    }
+  //check job
+  const job = await JobModel.findById(payload.jobId);
+  if (!job) {
+    throw new CustomError(404, "Job not found with the provided ID.");
+  }
 
-    const deadline = job.deadline;
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    deadline.setHours(0, 0, 0, 0);
+  //check cv is not uploaded
+  const myProfile = await CandidateModel.findOne({ userId: loginUserId });
+  if (!myProfile) {
+    throw new CustomError(404, "Data not found");
+  }
 
-    if (deadline < today) {
-        throw new CustomError(400, "The application deadline for this job has passed.");
-    }
+  if (!myProfile.cv) {
+    throw new CustomError(
+      400,
+      "You need to upload your CV before applying for a job."
+    );
+  }
 
-    //check already applied
-    const appliedJob = await ApplicationModel.findOne({
-        jobId: payload.jobId,
-        employerUserId: job.userId,
-        candidateUserId: loginUserId
-    })
+  const deadline = job.deadline;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  deadline.setHours(0, 0, 0, 0);
 
-    if(appliedJob){
-        throw new CustomError(409, 'You have already applied for this job.');
-    }
+  if (deadline < today) {
+    throw new CustomError(
+      400,
+      "The application deadline for this job has passed."
+    );
+  }
 
-    //create application
-    const result = await ApplicationModel.create({ 
-        jobId: payload.jobId,
-        employerUserId: job.userId,
-        candidateUserId: loginUserId
-    })
+  //check already applied
+  const appliedJob = await ApplicationModel.findOne({
+    jobId: payload.jobId,
+    employerUserId: job.userId,
+    candidateUserId: loginUserId,
+  });
 
-    return result;
-}
+  if (appliedJob) {
+    throw new CustomError(409, "You have already applied for this job.");
+  }
 
+  //create application
+  const result = await ApplicationModel.create({
+    jobId: payload.jobId,
+    employerUserId: job.userId,
+    candidateUserId: loginUserId,
+  });
+
+  return result;
+};
 
 export default ApplyJobService;
-
